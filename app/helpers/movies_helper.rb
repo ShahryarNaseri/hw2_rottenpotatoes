ad1ecd72f4e67036547950be49768c7d17080d80
Shahryar Naseri <shahryar_n2000@yahoo.com>
2012-06-18 00:49:03 -0700

    Modified movies_helper.rb

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,13 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  
+  def index_store_session params
+    @all_ratings = {'G' => 1, 'PG' => 1, 'PG-13' => 1, 'R' => 1}
+    session[:ratings] ||= @all_ratings
+    session[:ratings] = params[:ratings] if params[:ratings]
+    session[:by] = params[:by] if params[:by]
+    session[:order] = params[:order] if params[:order]
+  end
+  
 end
